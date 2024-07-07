2355a6b6a088c64d621e58eecfa17813d49e8bac
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-07-06 23:25:03 -0500

    changed to selenium instead of requests

--- python/chatbot.py
+++ python/chatbot.py
@@ -1,24 +1,30 @@
 from openai import OpenAI
 from bs4 import BeautifulSoup
-from typing import List, Set
-import argparse, requests, validators, time
+from typing import List, Set 
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import argparse, validators
 
 # Point to the local server
 client: OpenAI = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-parser = argparse.ArgumentParser(description="Enter your prompt.")
+fire_fox_options = Options()
+fire_fox_options.add_argument("--headless")
+driver = webdriver.Firefox(options=fire_fox_options)
 
-parser.add_argument("url", help="Type in a specific college url (home page)", type=str)
-parser.add_argument("interests", help="Type some of your interests.", type=str)
+parser = argparse.ArgumentParser(description="Enter your prompt.")
 parser.add_argument("college", help="Type in the college/university you are interested in.", type=str)
+parser.add_argument("interests", help="Type some of your interests in one string and separate interests by spaces.", type=str)
 parser.add_argument("MAX_URL_COUNT", help="Max amount of urls in the stack.", default=100, type=int)
 parser.add_argument("MAX_URL_COUNT_QUIT", help="Max amount of times the stack can have max amount of urls before stopping.", default=10, type=int)
 
 black_list: List[str] = ["youtube", "youtu.be", "facebook", "threads", "google", "calendar"]
 system_context: str = """
-Respond ONLY in plain text. 
+Respond ONLY in plain text. NO MARKDOWN. BE CONCISE.
 You need to help the user in finding relevant activities & opportunities a college has in a specific area. 
-Make sure to pay attention to specific attention to 'User interests: ...' at the bottom of the prompts and tailor your response to the interests.
- You will be given a bunch of text that has been found on the college's web pages.
+Make sure to pay attention to specific attention to 'User interests: ...' at the bottom of the prompts and tailor your response to the interests listed.
+If the dad is unrelated to the user interests, respond back with 'Unable to help you with the task.'
+DON'T BOTHER TO WRITE ABOUT SOMETHING UNRELATED.
+You will be given a bunch of text that has been found on the college's web pages.
 """
 prompt: str = ""
 response: str = ""
@@ -29,47 +35,53 @@ MAX_URL_COUNT = args.MAX_URL_COUNT
 MAX_URL_COUNT_QUIT = args.MAX_URL_COUNT_QUIT
 
 college: str = args.college.lower()
-url = args.url
 interests = args.interests
 
 def get_response() -> None:
     print(prompt)
     print("Generated Response:\n")
     completion = client.chat.completions.create(
-        model="LM Studio Community/Phi-3-mini-4k-instruct-GGUF",
+        model="RichardErkhov/fblgit_-_una-cybertron-7b-v1-fp16-gguf",
         messages=[
-        {"role": "system", "content": system_context},
+        {"role": "system", "content": " ".join(system_context.split("\n"))},
         {"role": "user", "content": prompt}
-        ],
-        temperature=0.7,
+        ]
     )
     response = completion.choices[0].message.content
     print(response)
 
-def parse_college(url: str, interests: str) -> None:
+def parse_college(college: str, interests: str) -> None:
     global prompt
-    prompt = get_all_content(url, interests)
+    prompt = get_all_content(college, interests)
     get_response()
 
-def get_all_content(initial_url: str, interests: str) -> str:
+def get_all_content(college: str, interests: str) -> str:
+    interests = interests.replace(' ', '+')
+    initial_url: str = f"https://www.google.com/search?q={college}+{interests}+opportunities"
+    driver.get(initial_url)
+
+    context: List[str] = []
+    context.append(f"User interests: {interests}\nEVERYTHING BELOW IS DATA.\n" + "-" * 50)
+
+    # To get the first url from the google search and go from there.
+    content: bytes = driver.page_source
+    soup = BeautifulSoup(content, "lxml")
+    initial_url = soup.find("a", attrs={"jsname": "UWckNb"}).get("href")
+ 
     count: int = 0
     # We only want to increment count once during one iteration.
     count_incremented: bool = False
-    context: List[str] = []
     stack: List[str] = []
     seen: Set[str] = set()
     stack.append(initial_url)
+    seen.add(initial_url)
 
     while stack:
         curr_url: str = stack.pop()
         print(curr_url, len(stack), count)
 
-        try:
-            content: bytes = requests.get(curr_url).content
-        except requests.exceptions.SSLError as e:
-            # We won't add back the url.
-            print(e)
-            continue
+        driver.get(curr_url)
+        content = driver.page_source
 
         soup = BeautifulSoup(content, "lxml")
         context.append(".\n".join(soup.get_text(separator=".", strip=True).split(".")))
@@ -85,7 +97,7 @@ def get_all_content(initial_url: str, interests: str) -> str:
         
         count_incremented = False
     
-    context.append("-" * 50 + f"\nEVERYTHING ABOVE IS DATA.\nUser interests: {interests}")
+    driver.quit()
     return "Context: " + "\n".join(context)
 
 def passed_link_check(link: str) -> bool:
@@ -102,4 +114,4 @@ def passed_link_check(link: str) -> bool:
         return False
     return True
 
-parse_college(url, interests)
+parse_college(college, interests)
