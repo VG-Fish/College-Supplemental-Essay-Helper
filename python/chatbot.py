fa37f78233b8e165bbf3788b5733621bd2e6f225
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-07-06 19:17:33 -0500

    made the outputted text more readable.

--- python/chatbot.py
+++ python/chatbot.py
@@ -1,17 +1,19 @@
 from openai import OpenAI
 from bs4 import BeautifulSoup
-from typing import List
+from typing import List, Set
 import argparse, requests, validators, re
 
 # Point to the local server
 client: OpenAI = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 parser = argparse.ArgumentParser(description="Enter your prompt.")
+
 parser.add_argument("url", help="Type in a specific college url (home page)", type=str)
 parser.add_argument("interests", help="Type some of your interests.", type=str)
+parser.add_argument("college", help="Type in the college/university you are interested in.", type=str)
 parser.add_argument("MAX_URL_COUNT", help="Max amount of urls in the stack.", default=100, type=int)
-parser.add_argument("MAX_URL_COUNT_QUIT", help="Max amount of times the stack can have max amount of urls before stopping.", default=3, type=int)
+parser.add_argument("MAX_URL_COUNT_QUIT", help="Max amount of times the stack can have max amount of urls before stopping.", default=10, type=int)
 
-black_list: List[str] = ["youtube", "youtu.be"]
+black_list: List[str] = ["youtube", "youtu.be", "facebook", "threads", "google"]
 prompt: str = ""
 response: str = ""
 
@@ -19,6 +21,8 @@ args = parser.parse_args()
 MAX_URL_COUNT = args.MAX_URL_COUNT
 # Max amount of times the stack can have max amount of urls before stopping.
 MAX_URL_COUNT_QUIT = args.MAX_URL_COUNT_QUIT
+
+college: str = args.college.lower()
 url = args.url
 interests = args.interests
 
@@ -38,31 +42,43 @@ def parse_college(url: str, interests: str) -> None:
     print(prompt)
 
 def get_all_content(initial_url: str, interests: str) -> str:
-    count = 0
+    count: int = 0
+    # We only want to increment count once during one iteration.
+    count_incremented: bool = False
     context: List[str] = []
-    stack = []
+    stack: List[str] = []
+    seen: Set[str] = set()
     stack.append(initial_url)
 
     while stack:
         curr_url = stack.pop()
+        print(curr_url, len(stack), count)
         content = requests.get(curr_url).content
         soup = BeautifulSoup(content, "html.parser")
-        context.append(" ".join(soup.get_text().split()) + ".")
-        
+        context.append("\n".join(soup.get_text(separator=".", strip=True).split(".")) + ".\n")
+
         for tags in soup.find_all("a"):
             link = tags.get("href")
-            if len(stack) <= MAX_URL_COUNT and passed_black_list_check(link):
-                stack.append(link)
-            if len(stack) == MAX_URL_COUNT:
+            if len(stack) == MAX_URL_COUNT and not count_incremented:
                 count += 1
+                count_incremented = True
+            if len(stack) <= MAX_URL_COUNT and link not in seen and passed_link_check(link):
+                stack.append(link)
+                seen.add(link)
         
-        if count == MAX_URL_COUNT_QUIT:
+        count_incremented = False
+        if count >= MAX_URL_COUNT_QUIT:
             break
     
-    context.append(f"{interests = }")
+    context.append(f"User interests: {interests}")
     return "Context: " + "\n".join(context)
 
-def passed_black_list_check(link: str) -> bool:
+def passed_link_check(link: str) -> bool:
+    if not link:
+        return False
+    elif college not in link:
+        return False
+    
     for b_l in black_list:
         if b_l in link:
             return False
